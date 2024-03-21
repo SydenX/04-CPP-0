327ddb0d04ebe8515c8ebd37d2f21c8eaa94aaa2
Jeremy Tollenaers <jtollena@shi-r4-p6.s19.be>
2024-03-21 13:14:54 +0100

    save

---
+++ ex01/main.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtollena <jtollena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/14 13:29:43 by jtollena          #+#    #+#             */
+/*   Updated: 2024/03/14 15:35:14 by jtollena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
+#include <iostream>
+#include "phonebook.hpp"
+#include "contact.hpp"
+#include <cstring>
+
+#endif
